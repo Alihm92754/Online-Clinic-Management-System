6ea36ec2b79d120a27d49a2b6e402890ee9045db
Alihm92754 <alihammoud92754@gmail.com>
2023-07-22 01:42:15 +0300

    last update

---
+++ dctrmenu.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body style="background-image:url(doctordesk.jpg)">
+<link rel="stylesheet" href="main.css">
+	<form action="mlogin.php" method="post">
+	<div class="header">
+				<ul>
+					<li style="float:left;border-right:none"><strong> Doctor Login</strong></li>
+					<li><a href="cover.php">Home</a></li>
+				</ul>
+	</div>
+	
+	
+    </form>
+	</body>
+	</html>
